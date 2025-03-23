2ea73203584f42111e45390d4d8f454316f8fa0f
Tanner <shiftseven@icloud.com>
2025-03-23 12:30:28 -0500

    initial set up push

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+  return {
+    templateFormats: ["njk", "md", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+    },
+  };
+};
